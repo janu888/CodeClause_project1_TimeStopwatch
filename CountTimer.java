93882eb3116939eb5ad215fbfb80ae4899ced454
Jahnavi <2110030287@klh.edu.in>
2023-07-30 19:26:31 +0530

    Add files via upload
    This page consists of a Timer code. It consists of three buttons start, reset, and back. When clicked on start it starts the countdown. Reset starts the counting from the start and back redirects you to the main page again.

---
+++ CountTimer.java
@@ -0,0 +1,155 @@
+package p1;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class CountTimer implements ActionListener {
+	
+    JFrame frame=new JFrame();
+	JButton startButton1=new JButton("START");
+	JButton resetButton=new JButton("RESET");
+	JButton back = new JButton("BACK");
+	JLabel timeLabel=new JLabel();
+	
+	int seconds=0;
+	int minutes=0;
+	int hours=1;
+	boolean started=false;
+	String seconds_string=String.format("%02d",seconds);
+	String minutes_string=String.format("%02d",minutes);
+	String hours_string=String.format("%02d",hours);
+	
+	Timer timer=new Timer(1000, new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			seconds--;
+			
+			seconds_string=String.format("%02d",seconds);
+			minutes_string=String.format("%02d",minutes);
+			hours_string=String.format("%02d",hours);
+			timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+			
+			if(seconds == -1) {
+				seconds=59;
+				minutes--;
+				
+				seconds_string=String.format("%02d",seconds);
+				minutes_string=String.format("%02d",minutes);
+				hours_string=String.format("%02d",hours);
+				timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+				
+			}
+			if(minutes == -1) {
+				minutes=59;
+				hours--;
+				
+				seconds_string=String.format("%02d",seconds);
+				minutes_string=String.format("%02d",minutes);
+				hours_string=String.format("%02d",hours);
+				timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+				
+			}
+			if (minutes==0 && seconds==0 && hours==0) {
+				timer.stop();
+				
+				
+			}
+	
+			
+			
+			
+			
+		}
+	});
+	
+	
+	CountTimer(){
+		timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+		timeLabel.setBounds(100, 100, 200, 100);
+		timeLabel.setFont(new Font("Verdana",Font.PLAIN,35));
+		timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+		timeLabel.setOpaque(true);
+		timeLabel.setHorizontalAlignment(JTextField.CENTER);
+		
+		startButton1.setBounds(100,200,100,50);
+		startButton1.setFont(new Font("Verdana",Font.PLAIN,20));
+		startButton1.setFocusable(false);
+		startButton1.addActionListener(this);
+		
+		resetButton.setBounds(200,200,100,50);
+		resetButton.setFont(new Font("Verdana",Font.PLAIN,20));
+		resetButton.setFocusable(false);
+		resetButton.addActionListener(this);
+		
+		back.setBounds(140,270,100,50);
+		back.setFont(new Font("Verdana",Font.PLAIN,20));
+		back.setFocusable(false);
+		back.addActionListener(this);
+		
+		
+		frame.add(startButton1);
+		frame.add(resetButton);
+		frame.add(back);
+		frame.add(timeLabel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420, 420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==startButton1) {
+			start();
+			if(started==false) {
+				started=true;
+				startButton1.setText("STOP");
+				start();
+			}
+			else {
+				started=false;
+				startButton1.setText("START");
+				stop();
+				
+			}
+			
+			
+			
+		}
+		if(e.getSource()==resetButton) {
+			started=false;
+			startButton1.setText("START");
+			reset();
+		}
+		if(e.getSource()==back) {
+			started=false;
+			startButton1.setText("BACK");
+			options opt=new options();
+		}
+		
+		
+	}
+	void start() {
+		timer.start();
+		
+	}
+	void stop()
+	{
+		timer.stop();
+		
+	}
+	void reset() {
+		timer.stop();
+		
+		seconds=0;
+		minutes=0;
+		hours=0;
+		seconds_string=String.format("%02d",seconds);
+		minutes_string=String.format("%02d",minutes);
+		hours_string=String.format("%02d",hours);
+		timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+		
+	}
+	
+
+}
